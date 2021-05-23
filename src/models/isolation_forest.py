d93ba13ecbb6a1ee7a46e9fd5011acc04aea345b
Zack Lundqvist <Zack.lundqvist@hotmail.com>
2021-05-23 18:05:24 +0200

    Use sparse matrix for isolation_forest

--- src/models/isolation_forest.py
+++ src/models/isolation_forest.py
@@ -1,11 +1,9 @@
 import argparse
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.neighbors import NearestNeighbors
-from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.ensemble import IsolationForest
 import multiprocessing
+from scipy import sparse
+import pandas as pd
 import math
 from util.eval_result import EvalResult
 
@@ -20,42 +18,46 @@ class IsolationForestModel:
             self.c_floor = args.c_floor
             self.c_ceiling = args.c_ceiling
 
-        df = data_set['df']
-        self.X = df.drop(['label'], axis=1).values
-        self.y = df['label'].values
+        # Convert matrix to sparse (way faster)
+        df = data_set['df'].drop(['label'], axis=1)
+        sparse_matrix = sparse.csr_matrix(df)
+        self.X_sparse = pd.DataFrame.sparse.from_spmatrix(sparse_matrix)
+        self.y = data_set['df']['label'].values
 
         self.n_jobs = self.calculate_njobs(df=df)
 
     def calculate_njobs(self, df):
         cpus = multiprocessing.cpu_count()
-        million_rows = max(math.floor(len(df) / 10**6), 1)
-        cpus = max(math.floor(cpus / million_rows), 2)
+        # Scale down with increasing data set
+        # Not needed when using sparse matrices
+        # million_rows = max(math.floor(len(df) / 10**6), 1)
+        # cpus = max(math.floor(cpus / million_rows), 3)
 
         print(f'[+] Using cores: {cpus}')
 
         return cpus
 
-
     def train_validate(self):
-
+        print('[+] Fitting')
         if self.cont_mode == 'custom':
             self.contamination = max(min(self.contamination, self.c_ceiling), self.c_floor)
-            isolation_forest = IsolationForest(n_estimators=self.n_estimators, contamination=self.contamination, random_state=0, n_jobs=self.n_jobs).fit(self.X)
+            isolation_forest = IsolationForest(n_estimators=self.n_estimators, contamination=self.contamination, random_state=0, n_jobs=self.n_jobs).fit(self.X_sparse)
 
         elif self.cont_mode == 'auto':
-            isolation_forest = IsolationForest(n_estimators=self.n_estimators, random_state=0, n_jobs=self.n_jobs).fit(self.X)
+            isolation_forest = IsolationForest(n_estimators=self.n_estimators, random_state=0, n_jobs=self.n_jobs).fit(self.X_sparse)
 
         elif self.cont_mode == 'exact':
-            isolation_forest = IsolationForest(n_estimators=self.n_estimators, contamination=self.contamination, random_state=0, n_jobs=self.n_jobs).fit(self.X)
+            isolation_forest = IsolationForest(n_estimators=self.n_estimators, contamination=self.contamination, random_state=0, n_jobs=self.n_jobs).fit(self.X_sparse)
 
-
-        y_pred = isolation_forest.predict(self.X)
+        print('[+] Predicting')
+        y_pred = isolation_forest.predict(self.X_sparse)
 
         y_pred = np.where(y_pred == -1, 'A', 'N')   # Get predicted labels
 
         eval_results = EvalResult(input_file=self.input_file, true_y=self.y, pred_y=y_pred)
         eval_results.pretty_print()
         return eval_results
+
 
     # Returns the static portion of the model id (filename not included)
     @staticmethod
